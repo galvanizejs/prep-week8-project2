649c9efbf10119f1fe62ffb8530cdf458c60776c
galvanizejs <js@matthewleach.com>
2016-08-29 11:32:59 -0600

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# prep-week8-project2 
+# Week8 Project1
+
+## jQuery
+
+Use your existing HTML skills to create a new web page with text, images, and links and apply at least 2 jQuery techniques to showcase user interaction. The overall presentation of this project is up to you. 
---
+++ main.js
@@ -0,0 +1,17 @@
+/*
+=======================================================
+
+    ** Week 7 - Project 1  **
+*** Ajax and JSON with OMDB ***
+
+This assignment is more open than the past assignments. 
+You are free to build a simple web page that interacts 
+with OMBD. It can simply display data within the HTML 
+page or buttons and text fields can be added to interact
+with the data and update the page accordingly. 
+
+Have fun! 
+ 
+=======================================================
+*/
+
